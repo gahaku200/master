34834779a2faf3169e7bf5806edf5c412ef86759
gahaku200 <coldfiledfos@yahoo.co.jp>
2020-11-05 23:36:53 +0900

    auth1

--- resources/js/components/Register.vue
+++ resources/js/components/Register.vue
@@ -3,24 +3,22 @@
     <main>
       <div class="container">
         <div class="row">
-          <div class="col-md-8 col-md-offset-2">
+          <div class="col-md-3"></div>
+          <div class="col-md-6">
             <div class="panel panel-default">
               <div class="panel-heading text-center">ユーザー登録</div>
 
               <div class="panel-body">
-                <form
-                  class="form-horizontal"
-                  ref="observer"
-                  action="/register"
-                  id="register"
-                  method="post"
-                  tag="form"
-                >
+                <ValidationObserver class="form-horizontal" ref="observer" action="/register" id="register" method="post" tag="form" @submit.prevent="register()" v-slot="{ invalid }">
+                  <input type="hidden" name="_token" :value="csrf" />
                   <div class="form-group">
                     <label for="name" class="col-md-4 control-label">名前</label>
 
                     <div class="col-md-6">
-                      <input v-model="name" name="name" type="text" class="form-control" />
+                      <validation-provider name="名前" rules="required|max:20" v-slot="{ errors }">
+                        <input v-model="name" name="name" type="text" class="form-control" />
+                        <div class="alert alert-danger" v-show="errors[0]">{{ errors[0] }}</div>
+                      </validation-provider>
                     </div>
                   </div>
 
@@ -28,7 +26,10 @@
                     <label for="email" class="col-md-4 control-label">メールアドレス</label>
 
                     <div class="col-md-6">
-                      <input v-model="email" name="email" type="email" class="form-control" />
+                      <validation-provider name="メールアドレス" rules="required|email" v-slot="{ errors }">
+                        <input v-model="email" name="email" type="email" class="form-control" />
+                        <div class="alert alert-danger" v-show="errors[0]">{{ errors[0] }}</div>
+                      </validation-provider>
                     </div>
                   </div>
 
@@ -36,12 +37,10 @@
                     <label for="password" class="col-md-4 control-label">パスワード</label>
 
                     <div class="col-md-6">
-                      <input
-                        v-model="password"
-                        name="password"
-                        type="password"
-                        class="form-control"
-                      />
+                      <validation-provider name="パスワード" rules="required|min:8|confirmed:password_confirmation" v-slot="{ errors }">
+                        <input v-model="password" name="password" type="password" class="form-control"/>
+                        <div class="alert alert-danger" v-show="errors[0]">{{ errors[0] }}</div>
+                      </validation-provider>
                     </div>
                   </div>
 
@@ -49,24 +48,23 @@
                     <label for="password-confirm" class="col-md-4 control-label">パスワード再確認</label>
 
                     <div class="col-md-6">
-                      <input
-                        v-model="password_confirmation"
-                        name="password_confirmation"
-                        type="password"
-                        class="form-control"
-                      />
+                      <validation-provider name="パスワード再確認" rules="required|min:8" vid="password_confirmation" v-slot="{ errors }">
+                        <input v-model="password_confirmation" name="password_confirmation" type="password" class="form-control"/>
+                        <div class="alert alert-danger" v-show="errors[0]">{{ errors[0] }}</div>
+                      </validation-provider>
                     </div>
                   </div>
 
-                  <div class="form-group">
-                    <div class="col-md-6 col-md-offset-4">
+                  <div class="form-group-register">
+                    <div>
                       <button type="submit" class="btn btn-primary">登録</button>
                     </div>
                   </div>
-                </form>
+                </ValidationObserver>
               </div>
             </div>
           </div>
+          <div class="col-md-3"></div>
         </div>
       </div>
     </main>
@@ -74,14 +72,52 @@
 </template>
 
 <script>
+import { ValidationProvider, ValidationObserver, extend } from "vee-validate";
+import { required, max, min, email, confirmed } from "vee-validate/dist/rules"; 
+extend("required", {
+  ...required,
+  message: "{_field_}は必須です"
+});
+extend("email", {
+  ...email,
+  message: "{_field_}はメールアドレス形式で入力してください"
+});
+extend("min", {
+  ...min,
+  message: "{_field_}は最低でも{length}文字入力してください"
+});
+extend("max", {
+  ...max,
+  message: "{_field_}は最大でも{length}文字までです"
+});
+extend("confirmed", {
+  ...confirmed,
+  message: "再確認パスワードと入力が一致していません"
+});
+
 export default {
+  components: {
+    ValidationProvider,
+    ValidationObserver,
+  },
   data() {
     return {
       name: "",
       email: "",
       password: "",
-      password_confirmation: ""
+      password_confirmation: "",
+      csrf: document
+        .querySelector('meta[name="csrf-token"]')
+        .getAttribute("content"),
     };
+  },
+  methods: {
+    async register() {
+      const isValid = await this.$refs.observer.validate();
+      if (isValid) {
+        document.querySelector("#register").submit();
+      }
+    }
   }
 };
 </script>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="/css/app.css" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
   </head>
